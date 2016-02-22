c5eec22379c7793b42294d972321352e42e8f0f7
Yuki Sakata <doragon3.0920@gmail.com>
2016-02-22 23:39:41 +0900

    TextFieldへの入力のさせ方、Data Pickerでの入力のさせ方検討

--- original/SecondViewController.swift
+++ original/SecondViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var lbDate: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // デートピッカー値変更時
+    @IBAction func changeDate(sender: UIDatePicker) {
+        
+        // 表示書式の設定
+        let df:NSDateFormatter = NSDateFormatter()
+        df.dateFormat = "yyyy年MM月dd日 HH時mm分"
+        
+        // 選択日時の表示
+        let mySelectedDate: NSString = df.stringFromDate(sender.date)
+        self.lbDate.text = mySelectedDate as String
+        
+    }
+    
 }
-
