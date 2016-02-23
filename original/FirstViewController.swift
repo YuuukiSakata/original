d9194fbf29327059928df7d3afc81abbf3ddb519
Yuki Sakata <doragon3.0920@gmail.com>
2016-02-23 17:56:51 +0900

    目標額、日付のラベル作成 Date Picker不明

--- original/FirstViewController.swift
+++ original/FirstViewController.swift
@@ -8,8 +8,30 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITextFieldDelegate {
+    
+    //目標額
+    @IBOutlet weak var moneyLabel: UILabel!
+    
+    //日付
+    @IBOutlet weak var datetext: UILabel!
+    @IBOutlet weak var picker: UIDatePicker!
 
+    // クリアボタンで実行されるtextFieldShouldClear()デリゲートメソッド
+    func textFieldShouldClear(textField: UITextField) -> Bool {
+        moneyLabel.text = "0"
+        return true
+    }
+    
+    /// Date Pickerの選択を変えたとき
+    @IBAction func valueChange(sender: AnyObject) {
+        let dt = picker.date
+        let fmt = NSDateFormatter()
+        fmt.dateFormat = "yyyy/MM/dd"
+        let str = fmt.stringFromDate(dt)
+        datetext.text = str
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
