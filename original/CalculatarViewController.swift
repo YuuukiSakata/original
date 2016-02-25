8362bc81a8f1de2ad03c3e3e8ec8bbbf031eef3b
Yuki Sakata <doragon3.0920@gmail.com>
2016-02-25 20:08:26 +0900

    電卓コーディングのみ

--- original/CalculatarViewController.swift
+++ original/CalculatarViewController.swift
@@ -9,15 +9,93 @@
 import UIKit
 
 class CalculatarViewController: UIViewController {
-
+    
+    
+    @IBOutlet var label: UILabel!
+    
+    
+    var number: Int = 0
+    var number2: Int = 0
+    var oparation: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func select1(){
+        number = number*10 + 1
+        label.text = String(number)
+    }
+    
+    @IBAction func select2(){
+        number = number*10 + 2
+        label.text = String(number)
+    }
+    
+    @IBAction func select3(){
+        number = number*10 + 3
+        label.text = String(number)
+    }
+    
+    @IBAction func select4(){
+        number = number*10 + 4
+        label.text = String(number)
+    }
+    
+    @IBAction func select5(){
+        number = number*10 + 5
+        label.text = String(number)
+    }
+    
+    @IBAction func select6(){
+        number = number*10 + 6
+        label.text = String(number)
+    }
+    
+    @IBAction func select7(){
+        number = number*10 + 7
+        label.text = String(number)
+    }
+    
+    @IBAction func select8(){
+        number = number*10 + 8
+        label.text = String(number)
+    }
+    
+    @IBAction func select9(){
+        number = number*10 + 9
+        label.text = String(number)
+    }
+    
+    
+    @IBAction func plus(){
+        //plusボタンを押した時に、labelに0が表示される
+        label.text = "0"
+        
+        oparation = 1
+        number2 = number
+        number = 0
+    }
+    
+    @IBAction func minus(){
+        label.text = "0"
+        
+        oparation = 2
+        number2 = number
+        number = 0
+    }
+    
+    @IBAction func clear(){
+        number == 0
+        label.text = String(number)
+    }
+    
+    @IBAction func equal(){
+        if oparation == 1{
+            label.text = String(number + number2)
+        }else if oparation == 2{
+            label.text = String(number2 - number)
+        }
     }
 }
